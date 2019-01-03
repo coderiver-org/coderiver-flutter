de165f633b4151e8dde8b42ff55613fabd09d166
君不见jj <18310600325@163.com>
2019-01-03 22:49:02 +0800

    登录界面

--- lib/ui/login/LoginPage.dart
+++ lib/ui/login/LoginPage.dart
@@ -1,6 +1,7 @@
 import 'package:coderiver/common/GlobalConfig.dart';
+import 'package:coderiver/common/Snack.dart';
 import 'package:coderiver/common/color_const.dart';
-import 'package:coderiver/utils/SizeUtil.dart';
+import 'package:coderiver/ui/mine/MinePage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,9 +15,6 @@ class _MarketPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        /*appBar: new AppBar(
-          title: new Text('登录', textAlign: TextAlign.right),
-        ),*/
         body: Container(
           alignment: AlignmentDirectional.topStart,
           padding: EdgeInsets.only(top: 40, left: 20, right: 20),
@@ -28,29 +26,128 @@ class _MarketPageState extends State<LoginPage> {
                 colors: [GREEN, GlobalConfig.colorPrimary]),
           ),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-          Padding(
-          padding: const EdgeInsets.fromLTRB(280.0, 10.0, 0.0, 0.0),
-          child: Text(
-            '登录',
-            style: TextStyle(
-                fontSize: 18,
-                inherit: false, //2.不继承默认样式
-                color: Colors.white),
-            //TextDirection.ltr从左至右，TextDirection.rtl从右至左
-            textDirection: TextDirection.rtl,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Align(
+                alignment: FractionalOffset.topRight,
+                child: new Padding(
+                  padding: EdgeInsets.only(right: 0.0, top: 0.0),
+                  child: new GestureDetector(
+                    child: new Text(
+                      '登录',
+                      style: new TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.white,
+                          fontStyle: FontStyle.normal),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  new LoginPage()));
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+                  child: new Image.asset(
+                    'images/logo.png',
+                    width: 90,
+                    height: 90,
+                  )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
+                child: new Text(
+                  '欢迎来到CodeRiver',
+                  style: new TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.white,
+                      fontStyle: FontStyle.normal),
+                ),
+              ),
+              RaisedButton.icon(
+                  color: Colors.white,
+                  disabledColor: Colors.transparent,
+                  shape: new RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(50))),
+                  icon: new Icon(Icons.star),
+                  onPressed: () {
+                    Snack.show(context, "666");
+                  },
+                  label: new Text('使用GitHub账号登录使用GitHub账号登录')),
+              RaisedButton.icon(
+                  color: Colors.transparent,
+                  disabledColor: Colors.transparent,
+                  onPressed: () {
+                    Snack.show(context, "777");
+                  },
+                  shape: new RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(50))),
+                  icon: new Icon(null),
+                  label: new Text(
+                    '创建账号创建账号创建账号创建账号创建账号',
+                    style: new TextStyle(
+                        color: Colors.white, fontStyle: FontStyle.normal),
+                  )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+                child: new Text(
+                  '更多登录方式',
+                  style: new TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontStyle: FontStyle.normal),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: RaisedButton.icon(
+                          color: Colors.white,
+                          disabledColor: Colors.transparent,
+                          icon: new Icon(Icons.star),
+                          label: new Text('新浪微博',
+                              style: new TextStyle(
+                                  color: Colors.white,
+                                  fontStyle: FontStyle.normal)))),
+                  Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: RaisedButton.icon(
+                          color: Colors.transparent,
+                          disabledColor: Colors.transparent,
+                          icon: new Icon(Icons.star),
+                          label: new Text(
+                            '微信',
+                            style: new TextStyle(
+                                color: Colors.white,
+                                fontStyle: FontStyle.normal),
+                          ))),
+                ],
+              )
+              //_body(),
+            ],
           ),
         ),
-        ImageIcon(
-          AssetImage('sources/images/logo.png'), color: Colors.white,)
-        //TopTitleBar(),
-        //_body(),
-        ],
       ),
-    ),)
-    ,
     );
   }
+}
+
+Widget _buildLoginBtn(BuildContext context) {
+  return RaisedButton(
+      padding: const EdgeInsets.all(8.0),
+      color: GlobalConfig.colorPrimary,
+      textColor: Colors.white,
+      child: Text("登录"),
+      elevation: 4.0,
+      onPressed: () {
+        Snack.show(context, "账号/密码不符合标准");
+      });
 }
